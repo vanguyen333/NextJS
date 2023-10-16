0b055f4a6aa85e13d17bcc7a9bb9d4ed130cc9fe
vanguyen333 <vanguyen333@yahoo.com>
2023-10-15 23:00:51 -0400

    added logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">Home</Link>
+      <Link href="/">
+        <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
     </nav>
   );
 };
