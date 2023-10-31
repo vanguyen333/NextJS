7f97d49b70ecbe23a01a250a811ea9e96b6419f5
vanguyen333 <vanguyen333@yahoo.com>
2023-10-31 15:52:50 -0400

    hero:added background and camp img

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 bb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+      <div className="hero-map " />
+      {/*LEFT  */}
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
