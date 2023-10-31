e69a91090504c0f882e872390fbcbcddf6586187
vanguyen333 <vanguyen333@yahoo.com>
2023-10-31 16:10:50 -0400

    hero added buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Button from "./Button";
 
 const Hero = () => {
   return (
@@ -20,6 +21,31 @@ const Hero = () => {
           seeing the incorruptible beauty of nature. We can help you on an
           adventure around the world in just one app
         </p>
+        <div className="my-11 flex flex-wrap gap-5">
+          <div className="flex items-center gap-2">
+            {Array(5)
+              .fill(1)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
+          </div>
+          <p className="bold-16 lg:bold-20 text-blue-70">
+            198k
+            <span className="regular-16 lg:regular-20 ml-1">
+              Exellent Reviews
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col w-full gap-3 sm:flex-row">
+          <Button type="button" title="Download App" variant="btn_green" />
+          <Button type="button" title="How we work?" variant="btn_white_text" />
+        </div>
       </div>
     </section>
   );
