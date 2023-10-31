02ab5cad8fa3506e7cbcd08c2edcf212b12740d5
vanguyen333 <vanguyen333@yahoo.com>
2023-10-31 16:54:34 -0400

    added 1 camp site img to the left

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,35 @@
-import React from "react";
-
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage}`}>
+      Camp Site 1
+    </div>
+  );
+};
 const Camp = () => {
-  return <div>Camp</div>;
+  return (
+    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite />
+      </div>
+    </section>
+  );
 };
 
 export default Camp;
