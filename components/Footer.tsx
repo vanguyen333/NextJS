c4b44328df3f81713ba208e9254762436fdbcd95
vanguyen333 <vanguyen333@yahoo.com>
2023-11-01 15:17:24 -0400

    done footer done page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,6 +52,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">
+          2023 Hilink | All rights reserved
+        </p>
       </div>
     </footer>
   );
