03130dcf13eaffdf3600bf97f6e0d952f5fc16ad
vanguyen333 <vanguyen333@yahoo.com>
2023-11-01 13:35:54 -0400

    remove border for the Camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -17,7 +17,7 @@ const CampSite = ({
     <div
       className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
     >
-      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500">
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 ">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
             <Image src="/folded-map.svg" alt="map" width={28} height={28} />
@@ -48,7 +48,7 @@ const CampSite = ({
 };
 const Camp = () => {
   return (
-    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <section className=" 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
